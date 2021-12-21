8b34e8bb61f33911bb8452db952a7fe9855235b7
Belous MacOS <conarti.dsn@gmail.com>
2021-12-21 19:06:42 +0300

    add yaml support

--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,9 +1,15 @@
+import yaml from 'js-yaml';
+
 const parseJson = (data) => JSON.parse(data);
+const parseYml = (data) => yaml.load(data);
 
 const parse = (buffer, extension) => {
 	switch (extension) {
 		case '.json':
 			return parseJson(buffer);
+		case '.yml':
+		case '.yaml':
+			return parseYml(buffer);
 		default:
 			throw new Error(`parse: Unsupported extension type - ${extension}`);
 	}
