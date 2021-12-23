3c6a22d75ef0c80dc3fc20808ef2b6c0b5f8e8d1
Belous MacOS <conarti.dsn@gmail.com>
2021-12-23 12:25:18 +0300

    remove tests for plain structures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,38 +10,21 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8').t
 
 const stylishResult = readFile('stylish.txt');
 const jsonResult = readFile('json.txt');
-const stylishNestedResult = readFile('stylishNested.txt');
-const jsonNestedResult = readFile('jsonNested.txt');
-const plainNestedResult = readFile('plainNested.txt');
+const plainResult = readFile('plain.txt');
 
-test('should works with plain json', () => {
+test('should works with json', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
 
   expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
   expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
+  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
   expect(() => genDiff(filepath1, filepath2, 'unsupported-format')).toThrow();
 });
 
-test('should works with plain yml', () => {
+test('should works with yml', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yml');
 
   expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
-  expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
-});
-
-test('should works with nested files', () => {
-  const filepath1 = getFixturePath('fileNested1.json');
-  const filepath2 = getFixturePath('fileNested2.json');
-
-  expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonNestedResult);
-  expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishNestedResult);
-});
-
-test('should works plain output', () => {
-  const filepath1 = getFixturePath('fileNested1.json');
-  const filepath2 = getFixturePath('fileNested2.json');
-
-  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainNestedResult);
 });
